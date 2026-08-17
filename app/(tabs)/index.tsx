bed3f59df433a3c75d9d532d99d7df360c065a13
Gustavo-Garshal <eternogustavo5@gmail.com>
2026-08-17 19:32:59 -0400

    aula01

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,65 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
-
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View style={styles.container}>
+      <Text style={styles.titulo}>Bem-vindo moreno</Text>
+      <Text style={styles.subtitulo}>Este é o seu aplicativo React Native!</Text>
+      <Text style={styles.corpo}>
+        O Lorem Ipsum é um texto modelo da indústria tipográfica 
+        e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias
+        desde o ano de 1500, quando uma misturou os caracteres de um texto para criar 
+        um espécime de livro. Este texto não só sobreviveu 5 séculos, 
+        mas também o salto para a tipografia electrónica, 
+        mantendo-se essencialmente inalterada. Foi popularizada nos anos 60 com a 
+        disponibilização das folhas de Letraset, que continham passagens com 
+        Lorem Ipsum, e mais recentemente com os programas de publicação 
+        como o Aldus PageMaker que incluem versões do Lorem Ipsum. 
+      </Text>
+      <TouchableOpacity style={{
+        backgroundColor: 'black',
+        padding: 10,
+        height: 'auto',
+        borderRadius: 20,
+        marginTop: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+      onPress={() => alert('Ai meu pézinho eduardo')}>
+        <Text style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>
+          Clique aqui pra uma
+        </Text>
+      </TouchableOpacity>
+      <Text style={{ color: '#f5f5f5', fontSize: 15, textAlign: 'center', marginVertical: 100}}>
+        Cuidado
+      </Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
     alignItems: 'center',
-    gap: 8,
+    justifyContent: 'center',
+    backgroundColor: '#3fa103',
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  titulo: {
+    color: '#ffffff',
+    fontSize: 28,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  subtitulo: {
+    color: '#f5f5f5',
+    fontSize: 20,
+    textAlign: 'center',
+  },
+  corpo: {
+    color: '#f5f5f5',
+    fontSize: 15,
+    textAlign: 'center',
+    marginVertical: 30,
+    marginHorizontal: 20,
   },
 });
